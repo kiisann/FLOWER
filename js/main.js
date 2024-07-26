b4012e0011bd1b8d65323f4d8cab138df5012acb
kiisann <fikisulistiawan@gmail.com>
2024-07-26 23:17:06 +0700

    mengubah nama

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('').split('')
+    const titles = ('Ni Kadek Diah Sekar Ayu').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
